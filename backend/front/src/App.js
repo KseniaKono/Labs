055b9d4c9f686dea6e33f7405d11bf8fb336b275
KseniaKono <100347521+KseniaKono@users.noreply.github.com>
2022-05-03 23:55:21 +0300

    9lab

--- backend/front/src/App.js
+++ backend/front/src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {createBrowserHistory} from "history";
 import NavigationBar from "./components/NavigationBar";
 import Home from "./components/Home";
+import Login from './components/Login';
+const API_URL = 'http://localhost:8081/api/v1'
+const AUTH_URL = 'http://localhost:8081/auth'
+
 function App() {
  return (
  <div className="App">
@@ -12,6 +16,8 @@ function App() {
  <div className="container-fluid">
  <Routes>
  <Route path="home" element={<Home />}/>
+ <Route path="Login" element={<Login />} />
+
  </Routes>
  </div>
  </BrowserRouter>
